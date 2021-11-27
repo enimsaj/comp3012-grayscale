cb7dcff0da85680397a8d3f8d4a219992492158f
J <jjillmago@gmail.com>
2021-11-27 12:00:14 -0800

    grayscale function

--- IOhandler.js
+++ IOhandler.js
@@ -11,8 +11,9 @@ const unzipper = require("unzipper"),
   fs = require("fs").promises,
   PNG = require("pngjs").PNG,
   path = require("path");
-  
-const createReadStream = require('fs').createReadStream;
+
+const { createReadStream, createWriteStream } = require("fs");
+
 
 /**
  * Description: decompress file from given pathIn, write to given pathOut
@@ -37,15 +38,16 @@ const unzip = (pathIn, pathOut) => {
  * @return {promise}
  */
 const readDir = (dir) => {
-  return new Promise((resolve)=>{
+  return new Promise((resolve, reject)=>{
     fs.readdir(dir)
     .then(filenames => {
       let images = [];
       for (let filename of filenames) {
-        if(filename.includes('.png')) images.push(filename);
+        if(path.extname(`${dir}/${filename}`) === ".png") images.push(filename);
       }
       resolve(images);
-    })    
+    })
+    .catch((e)=>reject(e))    
   })
 };
 
@@ -57,9 +59,33 @@ const readDir = (dir) => {
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {
-
-
+ const grayScale = (pathIn, pathOut) => {
+  
+  return new Promise(async(resolve, reject)=>{ 
+    createReadStream(pathIn)
+    .pipe(
+      new PNG({
+        filterType: 4,
+      })
+    )
+    .on("parsed",async function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+  
+          let gray = (this.data[idx] + this.data[idx+1] + this.data[idx+2])/3
+          
+          this.data[idx] = gray;
+          this.data[idx + 1] = gray;
+          this.data[idx + 2] = gray; 
+          
+        }
+      }
+      this.pack().pipe(createWriteStream(`${pathOut}`));
+      resolve(`${pathOut}`);
+    })
+    .on("error",reject)
+  })
 };
 
 module.exports = {
--- main.js
+++ main.js
@@ -15,7 +15,17 @@ const IOhandler = require("./IOhandler"),
 
 
   
-// IOhandler.unzip(zipFilePath, pathUnzipped)
- IOhandler.readDir(pathUnzipped)
- .then((data) => console.log(data))
- .catch();
+IOhandler.unzip(zipFilePath, pathUnzipped)
+ .then((pathUnzipped)=>IOhandler.readDir(pathUnzipped))
+ .then(async (data) => {
+    let i = 0;
+    await data.forEach((image) => {
+        let pathIn = `${pathUnzipped}/${image}`;
+        let pathOut = `${pathProcessed}/out${i++}.png`;
+        IOhandler.grayScale(pathIn,pathOut)
+        .then(()=>console.log("complete "+ pathOut))
+         .catch((err)=>console.log(err))
+      })
+    })
+    .catch((err)=>{console.log(err)});
+    
